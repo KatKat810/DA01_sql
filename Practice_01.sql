3f136bc2ceeb34eb1655e6e1f2e6f3bfba8f7c15
Katherine(NgaVo) <161305505+KatKat810@users.noreply.github.com>
2024-02-27 13:36:10 +0700

    Update and rename Practice_02.sql to Practice_01.sql

---
+++ Practice_01.sql
@@ -0,0 +1,14 @@
+-- Revising the Select Query II
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION >120000;
+
+---Japanese Cities' Names
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE ='JPN';
+
+--Weather Observation Station 1
+SELECT CITY, STATE
+FROM STATION;
+
+--Weather Observation Station 3
+
